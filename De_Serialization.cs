06cb3c2216109f56b30f8b18063e6787dd240293
Lewonchik1995 <t50_a.s.protsenko@mpt.ru>
2023-05-24 23:59:33 +0300

    Библиотека сериализации/десериализации XML

---
+++ De_Serialization.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Serialization;
+
+namespace De_SerializationLibrary
+{
+    public class De_Serialization
+    {
+        public static void Serialize<T>(T obj, string path)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                xml.Serialize(fs, obj);
+            }
+        }
+
+        public static T? Deserialize<T>(string path) 
+        {
+            T? obj;
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            using (var fs = new FileStream(path, FileMode.Open))
+            {
+                 obj = (T?)xml.Deserialize(fs);
+                
+            }
+            return obj;
+        }
+    }
+}
